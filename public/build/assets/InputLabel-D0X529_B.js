85609db7ea4d4a0d40ead313d9fc259df98b3f6f
dev-karunendu-mishra <166835818+dev-karunendu-mishra@users.noreply.github.com>
2026-01-07 21:06:00 +0530

    feat: Add new components and utilities for grid layout, transitions, and form handling
    - Introduced a new grid component with flexible layout options and responsive design.
    - Implemented transition utilities for smoother animations and state changes.
    - Added a checkbox component with bubble lock functionality for better user interaction.
    - Enhanced form handling capabilities with improved field instance management and scrolling behavior.

---
+++ public/build/assets/InputLabel-D0X529_B.js
@@ -0,0 +1,1 @@
+import{j as m}from"./app-BYa8S_9n.js";function o({value:t,className:e="",children:r,...s}){return m.jsx("label",{...s,className:"block text-sm font-medium text-gray-700 "+e,children:t||r})}export{o as I};
